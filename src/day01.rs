5536a8eb3831c0d4473b216241451ba842213dc3
Philip Weinke <philip.weinke@digitalnoise.de>
2022-12-01 11:22:15 +0100

    Refactor day 1 to use itertools

--- src/day01.rs
+++ src/day01.rs
@@ -1,15 +1,17 @@
 use std::fs;
 
+use itertools::Itertools;
+
 fn main() {
     let input = fs::read_to_string("input/day01.txt").unwrap();
 
-    let mut calories_per_elf: Vec<u32> = input
+    let calories_per_elf: Vec<u32> = input
         .split("\n\n")
         .map(|items| items.split("\n").map(|x| x.parse::<u32>().unwrap()).sum::<u32>())
+        .sorted()
+        .rev()
         .collect();
 
-    calories_per_elf.sort();
-
     println!("Part One: {}", calories_per_elf.iter().max().unwrap());
-    println!("Part Two: {}", calories_per_elf.iter().rev().take(3).sum::<u32>());
+    println!("Part Two: {}", calories_per_elf.iter().take(3).sum::<u32>());
 }
